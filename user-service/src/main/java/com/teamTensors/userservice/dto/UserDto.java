fb9fcc97bceb186b00681d37207b08ddff15f432
Chethana Jayasinghe <63340994+Chethana-it@users.noreply.github.com>
2023-08-19 00:20:20 +0530

    SQL integration with User Model

---
+++ user-service/src/main/java/com/teamTensors/userservice/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.teamTensors.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDto {
+    private int userId;
+    private String userName;
+    private String userAddress;
+    private String userNumber;
+}
---
+++ user-service/src/main/java/com/teamTensors/userservice/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.teamTensors.userservice.repo;
+
+import com.teamTensors.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+}
