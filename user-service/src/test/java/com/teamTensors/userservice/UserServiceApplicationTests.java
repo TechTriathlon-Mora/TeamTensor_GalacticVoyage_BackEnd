ac64c990f5eb51c3d0be2ad5ad91438ea6eb8140
NipunaNiranjan <nnnmsenanayakeg10@gmail.com>
2023-08-18 22:59:07 +0530

    add user service

---
+++ user-service/src/test/java/com/teamTensors/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamTensors.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
